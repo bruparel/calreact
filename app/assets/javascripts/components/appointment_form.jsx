605a29f3558c4b24a60a029443ed02873830bc2b
Bharat Ruparel <Bharat@wdvgco.com>
2017-10-14 12:44:11 -0500

    sample Rails 5.1.4 rep for Hrishi to look at

---
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -0,0 +1,40 @@
+var AppointmentForm = React.createClass({
+  handleChange: function(e) {
+    var name = e.target.name;
+    obj = {};
+    obj[name] = e.target.value;
+    this.props.onUserInput(obj);
+  },
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+    this.props.onFormSubmit();
+  },
+
+  setApptTime: function(e) {
+    var name = 'appt_time';
+    var obj = {};
+    if(obj[name] = e.toDate()) {
+      this.props.onUserInput(obj);
+    }
+  },
+  
+  render: function() {
+    var inputProps = {
+      name: 'appt_time'
+    };
+    
+    return (
+      <div>
+        <h2>Make a new appointment</h2>
+        <form onSubmit={this.handleSubmit}>
+          <input name='title' placeholder='Appointment Title'
+            value={this.props.input_title}
+            onChange={this.handleChange} />
+
+          <input type='submit' value='Make Appointment' />
+        </form>        
+      </div>
+    )
+  }
+});
